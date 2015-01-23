2558dcc8c9934e5da6815e6f9c9f95203881ee47
Altraya <phillippine.rousseau@iut-dijon.u-bourgogne.fr>
2015-01-23 22:05:58 +0100

    Debut de la todoliste. Juste un commentaire avec les spécifications.

---
+++ TODOListe.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+	Controleur de la page des todolists persos.
+
+RG1 : Les taches sont affichées par heure de début planifié puis par heure de fin planifiée
+RG2 : Le bouton commencé n'est disponible que pour les taches non commencées
+RG3 : Le bouton terminé n'est disponible que pour les taches en cours
+RG4 : Les tâches terminées ne peuvent pas être modifiées
+RG5 : Les items sont affichés avec la couleur de fond correspondante à leur catégorie (gestion
+de projet : violet, qualité : vert, sécurité : bleu, système et réseau : jaune, développement : orange)
+RG6 : Les poiroté MoSCoW doivent être affiches avec les couleurs suivantes : Must en rouge, Should en Orange,
+Could en vert et Would en bleu
+RG7 : pour chaque un picot peut apparaître : une coche verte pour les taches terminées,
+une horloge orange pour les taches en cours, pas de picot sinon
+RG8 : au clic sur le bouton commencer ou terminer, les heures réelles de début (pour commencer)
+ou de fin (pour terminer) sont mises a jour avec le timestamp correspondant au moment du clic. 
+
+*/
+?>
