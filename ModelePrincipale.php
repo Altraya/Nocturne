95fca161a00f25d94998a7f196c9c7996d74e29e
Cyhzault <Cyhzault@gmail.com>
2015-01-24 00:02:25 +0100

    Gestion de la connexion à la DB, n'a pas encore le mot de passe ROOT

--- ModelePrincipale.php
+++ ModelePrincipale.php
@@ -4,5 +4,16 @@
 	Modèle principal qui va servir à faire des requêtes dans tout le site.
 */
 
+//Permet de se connecter à la DB avec un PDO
+function void connexionBDD($bdd)
+{
+		try {
+
+		$bdd = new PDO('mysql:host=localhost;dbname=nuit_du_projet','root','');
+		
+	} catch (PDOException $e) {
+		die("Connexion à la base de donnée impossible:".$e->getMessage());
+	}
+}
 
 ?>
