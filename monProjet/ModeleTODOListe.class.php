497cafdbb661a98f131f22200d60a30e2c4d7530
Altraya <phillippine.rousseau@iut-dijon.u-bourgogne.fr>
2015-01-24 07:23:29 +0100

    Retablissement css et pleins de trucs.

--- monProjet/ModeleTODOListe.class.php
+++ monProjet/ModeleTODOListe.class.php
@@ -19,7 +19,7 @@ class ModeleTODOListe{
 	//Récupère l'état, l'item, la priorité, la tache, heure de début/fin planifié puis réel des taches d'une personne précise (connectée) donné en paramètre
 	public function getTaches($idPersonne){
 
-		$idPersonne = (string) $idPersonne;
+		$idPersonne = (int) $idPersonne;
 		$data = array();
 
 		$sql = "SELECT TSK_state, ITM_LIB, ITM_priority, TSK_LIB, TSK_start_hour_plan, TSK_end_hour_plan, TSK_start_hour_real, TSK_end_hour_real FROM TM_TASK_TSK INNER JOIN REF_ITEM_ITM ON FK_ITM = PK_ITM INNER JOIN TM_USER_USR ON FK_USR = PK_USR WHERE PK_USR = :idPersonne";
--- monProjet/TODOListe.php
+++ monProjet/TODOListe.php
@@ -36,29 +36,63 @@ ou de fin (pour terminer) sont mises a jour avec le timestamp correspondant au m
 
 	//si on est bien connecté, on recupere l'id nom prenom du membre co
 	if(isset($_SESSION['id'])){
-		var_dump($_SESSION['id']);
-		var_dump($_SESSION['prenom']);
-		var_dump($_SESSION['nom']);
-
+		$idUtilisateur = $_SESSION['id'];
 		$prenom = $_SESSION['prenom'];
 		$nom = $_SESSION['nom'];
 	
-
 		//affichage du titre sur la page
 		$vue->afficherDebutPage($prenom, $nom);
 
-		debutTableau();
-		$vue->titreTableauTodoList();
-		echo('<td>');
-		//debug
-		$idUtilisateur = $_SESSION['id'];
-
-
 		//recupere toutes les taches de l'utilisateur connecté
 		$taches = $modeleTODOListe->getTaches($idUtilisateur);
-		// debug var_dump($taches);
-		echo('ok cest bon');	
-		echo('</td>');
+		/*$etat = $taches[0]["TSK_state"];
+		$item = $taches[0]["ITM_LIB"];
+		$priorite = $taches[0]["ITM_priority"];
+		$tache = $taches[0]["TSK_LIB"];*/
+		$heureReelDebut = $taches[0]["TSK_start_hour_real"];
+
+		//affichage
+		debutTableau();
+			$vue->titreTableauTodoList();
+			echo('<tr>');
+				/*$vue->etat($etat);
+				$vue->item($item);
+				$vue->priorite($priorite);
+				$vue->tache($tache);*/
+
+				$vue->todoListSansHeuresReels($taches);
+
+				//coupe ce qui est donné en bdd pour avoir que l'heure
+				$heureExplode = explode(' ', $taches[0]["TSK_start_hour_real"]);
+				$heureExplode = $heureExplode[1]; //prend l'heure sous la force 03:00:00 //ceci est un string
+
+				$heureExplode = explode(':', $heureExplode); //divise en 3 => donne 03 00 00
+
+				//met l'heure en min pour qu'on puisse comparer
+				$heure = (int) $heureExplode[0];
+				$min = (int) $heureExplode[1];
+				$heureReelCompare =  '';
+				$heureReelCompare .= $heure;
+				$heureReelCompare .= ',';
+				$heureReelCompare .= $min;
+				//donne un string avec l'heure genre 3,0 pour pouvoir comparer
+
+				var_dump($heureReelCompare);
+
+				$heureExplode = $heureExplode[0] .'h'.$heureExplode[1].'';
+
+				var_dump($heureExplode);
+
+				//si on a deja commencé la tache donc que l'heure actuelle est apres l'heure de debut de la tache
+				//if($heureReelCompare < (string) date("H:i")){
+				if($debugRandom){
+
+					$vue->heureReelDebut($heureReelDebut);
+					
+				}
+
+				//$vue->ligneTableauTodoList($taches);
+			echo('</tr>');
 		finTableau();
 	}else{
 		errNonConnecte();
